ed3c7d8823895409404893d40b9ad5a66579704d
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2023-12-06 14:47:07 +0100

    created ch-java-2023-spring-web-asterix-api

---
+++ src/main/java/com/example/chjava2023springwebasterixapi/ChJava2023SpringWebAsterixApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chjava2023springwebasterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChJava2023SpringWebAsterixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChJava2023SpringWebAsterixApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chjava2023springwebasterixapi/ChJava2023SpringWebAsterixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chjava2023springwebasterixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChJava2023SpringWebAsterixApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
